980c15d15627250e2996b3d1c00dc1aa36fbf8bd
Cesar Gabriel <cesargabrielsousasousa196@gmail.com>
2023-06-27 07:07:56 -0300

    Mudanças na navegação

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let containerCards = document.getElementById('containerCards')
+let LeftNav = document.getElementById('LeftNav')
 
 const Card = (Materia) => `
     <div class="Card">
@@ -31,11 +32,67 @@ var Dados = {
     ]
 }
 
+function GetMaterias(Classe) {
+    var html = `<h1>Matematica</h1>`
+
+    Dados.Materias.forEach(Materia =>{
+        
+    })
+
+    return html
+}
+
+
 function CarregarClasses() {
-    containerCards.innerHTML = ""
-    Dados.Materias.forEach(Materia => {
-        containerCards.innerHTML += Card(Materia)
+    
+
+    var html = (Classe) =>`
+        <div class="OpcContainer">
+        
+            <li onclick="LoadPage('${Classe}');ShowMaterias('${Classe}')" data-Page="${Classe}" id="Opc">
+                <h1>
+                    <ion-icon name="school-outline"></ion-icon>
+                    ${Classe}
+                </h1>
+            </li>
+            <div id="Materias">
+                    ${GetMaterias()}
+            </div>
+        
+        </div>
+    `
+
+    Dados.Classes.forEach(Classe => {
+        LeftNav.innerHTML += html(Classe)
     })
 }
 
 CarregarClasses()
+
+function LoadPage(Page) {
+    let Opcs = document.querySelectorAll('#Opc')
+
+    Opcs.forEach(Opc => {
+        if (Opc.getAttribute('data-Page') == Page) {
+            Opc.classList.add('Select')
+        }else{
+            Opc.classList.remove('Select')
+        }
+    })
+}
+
+function ShowMaterias(Classe){
+    let Opcs = document.querySelectorAll('#Opc')
+
+    Opcs.forEach(Opc => {
+        if (Opc.getAttribute('data-Page') == Classe) {
+            if (Opc.parentElement.classList.contains('Aberto')) {
+                Opc.parentElement.classList.remove('Aberto')
+            }else {
+                Opc.parentElement.classList.add('Aberto')
+            }
+        }else{
+            Opc.parentElement.classList.remove('Aberto')
+        }
+    })
+}
